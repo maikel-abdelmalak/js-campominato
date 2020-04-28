bd2deeb7e82639eea423f2440df768b00513c205
maikel abdelmalak <maikel22@hotmail.it>
2020-04-28 12:33:37 +0200

    .

--- main.js
+++ main.js
@@ -22,7 +22,11 @@ max_numeri_pc = 16;
 var numeri_pc = [];
 
 for(var i = 0; i < max_numeri_pc; i++){
-    numeri_pc[i] = numero_random(minimo, massimo);
+    do{
+    var random = numero_random(minimo, massimo);
+}while(numeri_pc.includes(random))
+
+numeri_pc[i] = random;
 }
 
 console.log(numeri_pc);
@@ -31,9 +35,35 @@ var errore = false;
 
 var punteggio = 0;
 
-//con un ciclo while chiedo un numero nuovo numero all'utente fino a che l'utente non scelga uno dei numeri estratti dal pc
+//con un ciclo while chiedo un numero nuovo numero all'utente fino a che l'utente non scelga uno dei numeri estratti
+// while (errore == false){
+//     var numero_utente = inserimento_numero(minimo, massimo);
+//
+//     for (var i = 0; i < max_numeri_pc && errore == false; i++){
+//         if(numeri_pc[i] == numero_utente){
+//             errore = true;
+//         }
+//     }
+//     if(errore == false){
+//         punteggio++;
+//     }else{
+//         console.log('hai perso');
+//         console.log('il tuo puntegio è ' + punteggio);
+//     }
+//
+// }
+var array_utente = [];
+
 while (errore == false){
     var numero_utente = inserimento_numero(minimo, massimo);
+
+    while(array_utente.includes(numero_utente)){
+        alert('hai già inserito questo numero');
+        var numero_utente = inserimento_numero(minimo, massimo);
+    }
+
+    array_utente.push(numero_utente);
+
     for (var i = 0; i < max_numeri_pc && errore == false; i++){
         if(numeri_pc[i] == numero_utente){
             errore = true;
@@ -42,12 +72,17 @@ while (errore == false){
 
     if(errore == false){
         punteggio++;
-    }else{
-        console.log('hai perso');
-        console.log('il tuo puntegio è ' + punteggio);
     }
-
 }
+
+if(punteggio == (massimo - max_numeri_pc)){
+    console.log('hai vinto e hai raggiunto il punteggio massimo di  ' + punteggio );
+}else{
+    console.log('hai perso');
+    console.log('il tuo punteggio è ' + punteggio);
+}
+
+
 
 
 //FUNZIONI
